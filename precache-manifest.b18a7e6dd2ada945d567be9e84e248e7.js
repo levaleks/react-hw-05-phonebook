5364925f2bd6cad762cfbeb152b3a4ee71da31bc
Oleksii Levchenko <>
2020-10-06 00:50:05 +0300

    Updates

---
+++ precache-manifest.b18a7e6dd2ada945d567be9e84e248e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846956a5a9e88c8a9170dd201541fd6e",
+    "url": "/react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "d73b990d81da8d667d36",
+    "url": "/react-hw-05-phonebook/static/css/2.c98225b9.chunk.css"
+  },
+  {
+    "revision": "9e7068e8df3717fcec15",
+    "url": "/react-hw-05-phonebook/static/css/main.e4b1942e.chunk.css"
+  },
+  {
+    "revision": "d73b990d81da8d667d36",
+    "url": "/react-hw-05-phonebook/static/js/2.dcaa0df6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react-hw-05-phonebook/static/js/2.dcaa0df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7068e8df3717fcec15",
+    "url": "/react-hw-05-phonebook/static/js/main.daba90b3.chunk.js"
+  },
+  {
+    "revision": "64a39d755718f1ef423c",
+    "url": "/react-hw-05-phonebook/static/js/runtime-main.a988175b.js"
+  }
+]);
